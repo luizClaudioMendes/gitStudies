e7995dd7c9cceae7108310e01d64b8130836c3de
luiz mendes <luiz_claudio_cassio_mendes@hotmail.com>
2020-08-11 12:51:14 -0300

    dados de livro e usuario e squash de classe bibliotecario

--- src/biblioteca/Usuario.java
+++ src/biblioteca/Usuario.java
@@ -2,9 +2,15 @@ package biblioteca;
 
 public class Usuario {
 	
+<<<<<<< Upstream, based on master
 	public void cadastrar() {
 		
 	}
+=======
+	private int id;
+	private String nome;
+	
+>>>>>>> 0dd82fc dados de livro e usuario
 
 	public void excluir() {
 		
