7fcbfe489c66471ca10687fabf0a3974497bf8a9
luiz mendes <luiz_claudio_cassio_mendes@hotmail.com>
2020-08-12 11:41:30 -0300

    resolucao de conflito

--- src/biblioteca/Usuario.java
+++ src/biblioteca/Usuario.java
@@ -1,16 +1,14 @@
 package biblioteca;
 
 public class Usuario {
-	
-<<<<<<< Upstream, based on master
-	public void cadastrar() {
-		
-	}
-=======
+
 	private int id;
 	private String nome;
 	
->>>>>>> 0dd82fc dados de livro e usuario
+	public void cadastrar() {
+		
+	}
+
 
 	public void excluir() {
 		
